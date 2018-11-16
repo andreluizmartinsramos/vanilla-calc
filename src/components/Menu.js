3c1f2d939ddb8333e07c33cd5eb314a75f825dda
ANDRE LUIZ MARTINS RAMOS <andreluiz@sistemafiesc.org.br>
2018-11-16 17:28:48 -0200

    adjusting menu hamburguer

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,41 +4,40 @@
  * @description Class to manage all behavior of menu application
  */
 class Menu {
-
   /**
    * Method to initialize the Menu
    * @function init
    * @description Apply the listiners on the apropriate elements
    * */
-    init() {
-        document.querySelector('.wrapper-page__hamburguer-button').addEventListener('click', (e) => {
-            this.openMenu();
-        });
+  init() {
+    document.querySelector('.nav__hamburguer-button').addEventListener('click', (e) => {
+      this.openMenu();
+    });
 
-        document.querySelector('.hamburguer-menu__close').addEventListener('click', (e) => {
-            this.closeMenu();
-        });
-    }
+    document.querySelector('.hamburguer-menu__close').addEventListener('click', (e) => {
+      this.closeMenu();
+    });
+  }
 
   /**
    * Open Menu
    * @function openMenu
    * @description Manage the toggle of menu visibility/slide (Modifier --is-open)
    * */
-    openMenu() {
-        document.querySelector('.hamburguer-menu').classList.remove('hamburguer-menu--is-close');
-        document.querySelector('.hamburguer-menu').classList.add('hamburguer-menu--is-open');
-    }
+  openMenu() {
+    document.querySelector('.hamburguer-menu').classList.remove('hamburguer-menu--is-close');
+    document.querySelector('.hamburguer-menu').classList.add('hamburguer-menu--is-open');
+  }
 
   /**
    * Close Menu
    * @function closeMenu
    * @description Manage the toggle of menu visibility/slide (Modifier --is-close)
    * */
-    closeMenu() {
-        document.querySelector('.hamburguer-menu').classList.remove('hamburguer-menu--is-open');
-        document.querySelector('.hamburguer-menu').classList.add('hamburguer-menu--is-close');
-    }
+  closeMenu() {
+    document.querySelector('.hamburguer-menu').classList.remove('hamburguer-menu--is-open');
+    document.querySelector('.hamburguer-menu').classList.add('hamburguer-menu--is-close');
+  }
 }
 
 export default Menu;
